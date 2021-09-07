03937ece47066376a64b1f7525676163cb5c0b14
DZorikhin <zorikhin@yandex.com>
2021-09-07 11:38:06 +0200

    add files to repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+
+Website blog application has been developed using javascript, express.js and MongoDB.
+Interactive web app could be found
+[here.](https://complexappadvisorenergy.herokuapp.com/)
--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use('/api', require('./router-api'))
 
 let sessionOptions = session({
-    secret: 'JavaScript is sooo cooool',
+    secret: '',
     store: new MongoStore({client: require('./db')}),
     resave: false,
     saveUninitialized: false,
